241e337ac230757d4190d3eabba2cd10f89b0ca7
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-05-26 12:59:50 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn={ firstName:'Anas',lastName:"",email:'mdanas.2906@gmail.com'};
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+            <HeaderBox
+            type ="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+            />
+            <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
+            </header>
+            Recent Transactions
+        </div>
+        <RightSidebar
+         user={loggedIn} 
+         transactions={[]}
+         banks={[]}
+        />
+    </section>
   )
 }
 
---
+++ components/PlaidLink.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaidLink = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlaidLink
